306f3b4967776f0116c66a786dccf3a80354af01
Benno <58740254+BennoCraft@users.noreply.github.com>
2020-10-25 13:30:55 +0100

    Oceanman Command Uh

---
+++ commands/oceanman.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'oceanman',
+    usage: '',
+    guildOnly: false,
+    enabled: true,
+    level: 0,
+    aliases: [],
+    category: 'Misc',
+    description: 'Just try it',
+    moreHelp: null
+};
+// eslint-disable-next-line no-unused-vars
+module.exports.run = async (client, message, args, level) => {
+    message.channel.send('OCEAN MAN 🌊 😍 Take me by the hand ✋ lead me to the land that you understand 🙌 🌊 OCEAN MAN 🌊 😍 The voyage 🚲 to the corner of the 🌎 globe is a real trip 👌 🌊 OCEAN MAN 🌊 😍 The crust of a tan man 👳 imbibed by the sand 👍 Soaking up the 💦 thirst of the land 💯');
+
+};
